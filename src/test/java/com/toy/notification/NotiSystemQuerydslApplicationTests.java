1ccd21446d96b4182a07f0e298a6c8f41dc09429
Minjae Kim <minjae.kim@mytrum.com>
2022-03-08 22:01:42 +0900

    누락 사항 반영

--- src/test/java/com/toy/notification/NotiSystemQuerydslApplicationTests.java
+++ src/test/java/com/toy/notification/NotiSystemQuerydslApplicationTests.java
@@ -1,12 +1,14 @@
 package com.toy.notification;
 
+import com.toy.notification.domain.company.Company;
 import com.toy.notification.domain.company.CompanyRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
@@ -17,8 +19,8 @@ class NotiSystemQuerydslApplicationTests {
 
     @Test
     public void companySaveTest() {
-      
-       Company company = companyRepository.save(Company.builder()
+
+        Company company = companyRepository.save(Company.builder()
                 .name("test").build());
 
         List<Company> companyList = companyRepository.findAllCompany();
