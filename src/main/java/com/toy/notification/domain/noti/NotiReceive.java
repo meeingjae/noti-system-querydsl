41daf57f9d3a98b7f95b5a08c304a20e479a2848
Minjae Kim <minjae.kim@mytrum.com>
2022-03-09 17:18:31 +0900

    Hibernate 일대다 양방향 관계 구현 및 테스트 생성 - {4}

--- src/main/java/com/toy/notification/domain/noti/NotiReceive.java
+++ src/main/java/com/toy/notification/domain/noti/NotiReceive.java
@@ -1,13 +1,25 @@
 package com.toy.notification.domain.noti;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "NOTI_RECEIVE")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
 public class NotiReceive {
 
     @Id
@@ -15,12 +27,14 @@ public class NotiReceive {
     @Column(name = "noti_receive_id", nullable = false)
     private long notiReceiveId;
 
-    @Column(name = "noti_id", nullable = false)
-    private long notiId;
-
     @Column(name = "user_id", nullable = false)
     private long userId;
 
-    @Column(name = "read_flag", nullable = false)
-    private boolean readFlag;
+    @Column(name = "read_flag", nullable = true)
+    private Boolean readFlag;
+
+    // Noti Message를 가져오기 위해 EAGER로 설정했으나, Noti Message가 필요 없는 상황 발생 시 디버깅 필요
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "noti_id")
+    private Noti noti;
 }
--- src/test/java/com/toy/notification/NotiSystemQuerydslApplicationTests.java
+++ src/test/java/com/toy/notification/NotiSystemQuerydslApplicationTests.java
@@ -2,12 +2,21 @@ package com.toy.notification;
 
 import com.toy.notification.domain.company.Company;
 import com.toy.notification.domain.company.CompanyRepository;
+import com.toy.notification.domain.noti.Noti;
+import com.toy.notification.domain.noti.NotiReceive;
+import com.toy.notification.domain.noti.NotiReceiveRepository;
+import com.toy.notification.domain.noti.NotiRepository;
+import com.toy.notification.domain.user.User;
+import com.toy.notification.domain.user.UserRepository;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceUtil;
+import javax.persistence.metamodel.Metamodel;
 import java.util.List;
 
 @SpringBootTest
@@ -16,6 +25,18 @@ class NotiSystemQuerydslApplicationTests {
 
     @Autowired
     private CompanyRepository companyRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private NotiRepository notiRepository;
+
+    @Autowired
+    private NotiReceiveRepository notiReceiveRepository;
+
+    @Autowired
+    private EntityManagerFactory factory;
 
     @Test
     public void companySaveTest() {
@@ -30,4 +51,81 @@ class NotiSystemQuerydslApplicationTests {
         assert companyList.size() == 1;
     }
 
+    @Test
+    public void userExistQueryTest() {
+
+        userRepository.save(User.builder()
+                .companyId(1L)
+                .userName("ming")
+                .build());
+
+        System.out.println("Jpa Method Query");
+        boolean first = userRepository.existsById(1L);
+        System.out.println("querydsl limit query");
+        boolean second = userRepository.existByUserId(1L);
+
+        assert first;
+        assert second;
+    }
+
+    @Test
+    public void persisteneceContextTest() {
+
+        System.out.println("First save");
+        User user = userRepository.save(User.builder()
+                .companyId(1L)
+                .userName("ming")
+                .build());
+
+        user.setUserName("changedming");
+
+        System.out.println("Second save");
+        userRepository.save(user);
+    }
+
+    @Test
+    public void oneToManyTest() {
+
+        // Entity Load 정보 조회
+        PersistenceUtil persistenceUtil = factory.getPersistenceUnitUtil();
+
+        //모든 Entity 조회
+        Metamodel metamodel = factory.getMetamodel();
+
+        System.out.println(metamodel.getEntities());
+
+        Noti noti = notiRepository.save(
+                Noti.builder()
+                        .message("test Mesage")
+                        .userId(99L)
+                        .build());
+
+        List<NotiReceive> notiReceiveList = notiReceiveRepository.saveAllAndFlush(
+                List.of(
+                        NotiReceive.builder()
+                                .userId(100L)
+                                .noti(noti)
+                                .build(),
+                        NotiReceive.builder()
+                                .userId(101L)
+                                .noti(noti)
+                                .build())
+        );
+
+        noti.setNotiReceiveList(notiReceiveList);
+
+        noti = notiRepository.save(noti);
+
+        System.out.println("noti initialize ? ");
+        System.out.println(persistenceUtil.isLoaded(noti));
+
+        System.out.println("noti receive initialize ? ");
+        System.out.println(persistenceUtil.isLoaded(noti, "notiReceiveList"));
+
+//        noti.getNotiReceiveList()
+//                .forEach(chield ->
+//                        System.out.println(""
+//                                + "chield Id : " + chield.getUserId() +
+//                                " parentId : " + chield.getNoti().getNotiId()));
+    }
 }
